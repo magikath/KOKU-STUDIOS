4111b7c581e8c8dcc5c7855a0f340b9c7faae5ad
k-oku <kmoku@wisc.edu>
2023-01-19 17:34:27 -0600

    parallax banner :)

---
+++ index.js
@@ -0,0 +1,50 @@
+
+
+// let background = document.getElementById("background").innerHTML;
+// let koku = document.getElementById("koku").innerHTML;
+// let doodles = document.getElementById("doodles").innerHTML;
+// let me = document.getElementById("me").innerHTML;
+
+// let btn = document.getElementById("btn-letsgo");
+// let header = document.querySelector("header");
+
+
+// console.log(me);
+// console.log(background);
+// console.log(koku);
+// console.log(document.getElementById("koku"));
+
+// window.addEventListener("scroll", function () {
+//   let value = window.scrollY;
+//   console.log(value);
+//   me.style.top = value * 0.5 + "px";
+//   doodles.style.top = value * 0.25 + "px";
+//   koku.style.top = value * 1.05 + "px";
+// //   background.style.top = value * 0 + "px";
+//   //btn.style.marginTop = value * 1 + "px";
+//   header.style.top = value * 0.5 + "px";
+
+//   console.log(value);
+//   console.log("me");
+//   console.log(me.style.top);
+//   console.log("doodles");
+//   console.log(doodles.style.top);
+//   console.log("koku");
+//   console.log(koku.style.top);
+//   console.log("background");
+//   console.log(background.style.top);
+// });
+
+
+// window.onscroll = function() {myFunction()};
+                
+//                 var navbar = document.getElementById("navbar");
+//                 var sticky = navbar.offsetTop;
+                
+//                 function myFunction() {
+//                   if (window.pageYOffset >= sticky) {
+//                     navbar.classList.add("sticky")
+//                   } else {
+//                     navbar.classList.remove("sticky");
+//                   }
+//                 }
